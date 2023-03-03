0312c7ccd8175584db08d66412b66e01f316e61a
devinjc213 <devinjc213@gmail.com>
2023-03-03 17:09:02 -0600

    flood fill algo working

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -5,9 +5,20 @@ import { Icons } from '../assets/Icons';
 import ControlImage from '../components/ControlImage';
 import styles from './Canvas.module.css';
 
+type Pos = {
+  x: number
+  y: number
+}
+
+type RGB = {
+  r: number
+  g: number
+  b: number
+}
+
 const Canvas: Component<{ socket: Socket, isDrawer: boolean }> = (props) => {
   const [paintTool, setPaintTool] = createSignal<"brush" | "bucket">("brush");
-	const [drawColor, setDrawColor] = createSignal<string>("#000");
+	const [drawColor, setDrawColor] = createSignal<string>("#00000");
 	const [brushSize, setBrushSize] = createSignal<number>(5);
 	const [pos, setPos] = createSignal<{ x: number, y: number}>({x:0,y:0});
 	let rect: any;
@@ -15,14 +26,17 @@ const Canvas: Component<{ socket: Socket, isDrawer: boolean }> = (props) => {
 	let ctx: any;
 	let lastX: number;
 	let lastY: number;
+  let imageData: any;
 
 	onMount(() => {
     ctx = canvas.getContext("2d");
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.imageSmoothingEnabled = false;
 
     document.addEventListener('mousemove', draw);
     document.addEventListener('mousedown', handlePos);
+    document.addEventListener('mousedown', bucket);
     document.addEventListener('mouseup', clearPos);
     document.addEventListener('mouseEnter', handlePos);
   });
@@ -30,6 +44,7 @@ const Canvas: Component<{ socket: Socket, isDrawer: boolean }> = (props) => {
 	onCleanup(() => {
     document.removeEventListener('mousemove', draw);
     document.removeEventListener('mousedown', handlePos);
+    document.removeEventListener('mousedown', bucket);
     document.removeEventListener('mouseup', clearPos);
     document.removeEventListener('mouseEnter', handlePos);
   });
@@ -45,15 +60,19 @@ const Canvas: Component<{ socket: Socket, isDrawer: boolean }> = (props) => {
 
 	const handlePos = (e: any) => {
 		rect = canvas.getBoundingClientRect();
+    
+    let x = Math.floor((e.clientX - rect.left) / (rect.right - rect.left) * canvas.width);
+    let y = Math.floor((e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height);
 
-		setPos({
-			x: (e.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
-			y: (e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
-		});
-	}
+		setPos({ x, y });
+	};
+
+  const outOfBounds = (x: number, y: number) => { 
+    return ((x > canvas.width || x < 0) || (y > canvas.height || y < 0));
+  }
 
 	const draw = (e: any) => {
-		if (e.buttons !== 1 || !props.isDrawer) return;
+		if (e.buttons !== 1 || paintTool() === "bucket" || outOfBounds(pos().x, pos().y)) return;
 		ctx.beginPath();
 
 		ctx.lineWidth = brushSize();
@@ -68,6 +87,92 @@ const Canvas: Component<{ socket: Socket, isDrawer: boolean }> = (props) => {
 
 		props.socket.emit('canvas_emit', { x: pos().x, y: pos().y, color: drawColor(), brushSize: brushSize(), id: props.socket.id });
 	};
+
+  const hexToRgb = (hex: string) => {
+    return {
+      r: parseInt(hex.slice(1, 3), 16),
+      g: parseInt(hex.slice(3, 5), 16),
+      b: parseInt(hex.slice(5, 7), 16)
+    };
+  }
+
+  const colorMatch = (a: RGB, b: RGB) =>
+    a.r === b.r && a.g === b.g && a.b === b.b;
+
+  const getColorAtPos = (imageData: any, x: number, y: number) => {
+    const {width, data} = imageData;
+
+    return {
+      r: data[4 * (width * y + x)],
+      g: data[4 * (width * y + x) + 1],
+      b: data[4 * (width * y + x) + 2],
+    }
+  }
+
+  const setColorAtPos = (imageData: any, color: RGB, x: number, y: number) => {
+    const {width, data} = imageData;
+
+    data[4 * (width * y + x)] = color.r;
+    data[4 * (width * y + x) + 1] = color.g;
+    data[4 * (width * y + x) + 2] = color.b;
+  }
+
+  const bucket = (e: any) => {
+    if (paintTool() !== "bucket") return;
+    imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    handlePos(e);
+    const clickedColor = getColorAtPos(imageData, pos().x, pos().y);
+    const bucketColor = hexToRgb(drawColor());
+    let pixel: Pos;
+    const stack: Pos[] = [];
+
+    if (colorMatch(clickedColor, bucketColor) || outOfBounds(pos().x, pos().y)) return;
+
+    stack.push({ x: pos().x, y: pos().y });
+
+    while (stack.length) {
+      pixel = stack.pop()!;
+      let continueDown = true;
+      let continueUp = true;
+      let continueLeft = false;
+      let continueRight = false;
+
+      while (continueUp && pixel.y >= 0) {
+        pixel.y--;
+        continueUp = colorMatch(getColorAtPos(imageData, pixel.x, pixel.y), clickedColor);
+      }
+
+      while (continueDown && pixel.y < canvas.height) {
+        setColorAtPos(imageData, bucketColor, pixel.x, pixel.y);
+      
+        if (pixel.x - 1 >= 0
+          && colorMatch(getColorAtPos(imageData, pixel.x - 1, pixel.y), clickedColor)) {
+            if (!continueLeft) {
+              continueLeft = true;
+              stack.push({ x: pixel.x - 1, y: pixel.y });
+            }
+        } else {
+          continueLeft = false;
+        }
+
+        if (pixel.x + 1 < canvas.width
+            && colorMatch(getColorAtPos(imageData, pixel.x + 1, pixel.y), clickedColor)) {
+          if (!continueRight) {
+            stack.push({ x: pixel.x + 1, y: pixel.y });
+            continueRight = true;
+          }
+        } else {
+          continueRight = false;
+        }
+
+        pixel.y++
+        continueDown = colorMatch(getColorAtPos(imageData, pixel.x, pixel.y), clickedColor);
+      }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+  }
+  
 
 	const clearPos = () => {
 		setPos({ x: 0, y: 0 });
